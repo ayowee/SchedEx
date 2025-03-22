d769e916ccdf156caf827ff7af2994129a8308e1
Ayomal <pasinduayomal2001@gmail.com>
2025-03-22 23:22:41 +0530

    React schedular updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'; // Tailwind CSS
+import '@syncfusion/ej2-base/styles/material.css'; // Syncfusion base styles
+import '@syncfusion/ej2-react-schedule/styles/material.css'; // Syncfusion schedule styles
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
